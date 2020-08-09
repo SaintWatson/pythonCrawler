4736c0334facb3ec482b4628234bb7820291e7a9
Watson <a0916228544@gmail.com>
2020-08-09 21:09:29 +0800

    2020.08.09 update

--- Day13/homework.py
+++ Day13/homework.py
@@ -2,26 +2,22 @@ import requests
 url = 'https://www.ptt.cc/bbs/PC_Shopping/index.html'
 response = requests.get(url).text
 
-import re
-regex = '/bbs/PC_Shopping/M\.\d{10}.A.\w{3}\.html'
-rule = re.compile(regex)
-def is_article(url):
-    if url is None:
-        return False
-    if rule.match(url):
-        return True
-    else:
-        return False
-
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(response, features='html.parser')
 authors = soup.find_all('div', class_='author')
 titles = soup.find_all('div', class_='title')
 dates = soup.find_all('div', class_='date')
 
-articles = len(authors)    
+articles = len(authors)
+article_list = [['作者','標題','日期']]    
 for i in range(articles):
-    author = authors[i].text
-    title = titles[i].text
-    date = dates[i].text
-    print(f'{title[:-1]}---{date}: {author}')
+    author = authors[i].text.replace('\n','')
+    title = titles[i].text.replace('\n','')
+    date = dates[i].text.replace('\n','')
+    article_list.append([date, author, title])
+
+import csv
+filename = 'ptt.csv'
+with open(filename, 'w', encoding='Big5') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(article_list)
