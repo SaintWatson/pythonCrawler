2692869ec45ffd8030a54c3ec33047618e95096b
Watson <a0916228544@gmail.com>
2020-08-10 19:08:11 +0800

    2020.08.10 update

--- Day13/homework.py
+++ Day13/homework.py
@@ -1,23 +1,37 @@
 import requests
-url = 'https://www.ptt.cc/bbs/PC_Shopping/index.html'
+url = 'https://www.ptt.cc/bbs/NBA/index.html'
 response = requests.get(url).text
+print('Receive the response...')
 
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(response, features='html.parser')
 authors = soup.find_all('div', class_='author')
 titles = soup.find_all('div', class_='title')
 dates = soup.find_all('div', class_='date')
+print('Grasp the target infomation...')
 
 articles = len(authors)
-article_list = [['作者','標題','日期']]    
+article_list = [['作者','標題','日期']]   
 for i in range(articles):
     author = authors[i].text.replace('\n','')
     title = titles[i].text.replace('\n','')
     date = dates[i].text.replace('\n','')
     article_list.append([date, author, title])
 
+import time
+curTime = time.ctime().split()
+now = f'{curTime[1]}.{curTime[2]} {curTime[3]}'.replace(':','_')
+
+import os
+dirname = 'result'
+filename = now + '.csv'
+if dirname not in os.listdir():
+    os.mkdir(dirname)
+print('Create the table...')
+
 import csv
-filename = 'ptt.csv'
-with open(filename, 'w', encoding='Big5') as csvfile:
+fullname = dirname + '/' + filename
+with open(fullname, 'w', encoding='Big5') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerows(article_list)
+print(f'Done!!! \nThe file {filename} has saved in the directory {dirname}.')
